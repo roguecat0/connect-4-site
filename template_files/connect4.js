615e74849896f8ce787d698530e97f62d7a2f4eb
roguecat0 <roguecat0@gmail.com>
2024-10-22 18:50:55 +0200

    added response handeling for when backend added

--- template_files/connect4.js
+++ template_files/connect4.js
@@ -1,9 +1,23 @@
 columns = [0, 0, 0, 0, 0, 0, 0]
 turn = 1
+moves = ""
+
+const GameState = {
+    Draw: 0,
+    Win: 1,
+    Ongoing: 2
+}
+// response json
+response = {
+    "cpu_move": 0,
+    "game_state_pre": GameState.Ongoing,
+    "game_state_post": GameState.Win
+}
 
 $(document).ready(function () {
     $(".column").on("click", function () {
         play_move(this.id[1])
+        handel_response()
     })
 })
 
@@ -12,7 +26,6 @@ function play_move(eid) {
     if (sum > 5) {
         return
     }
-
     let column_row = `#c${eid}r${sum}`
     if (turn % 2 != 0) {
         $(column_row).css("background-color", "red")
@@ -21,6 +34,30 @@ function play_move(eid) {
         $(column_row).css("background-color", "yellow")
         $("#whosturn").text("Yellow's turn")
     }
+    moves = moves + eid
+    console.log(moves)
     columns[eid]++
     turn++
+}
+function handel_response() {
+    if (response["game_state_pre"] != GameState.Ongoing) {
+        show_winner(response["game_state_pre"])
+        return
+    }
+
+    play_move(response["cpu_move"])
+
+    if (response["game_state_post"] != GameState.Ongoing) {
+        show_winner(response["game_state_post"])
+    }
+}
+function show_winner(state) {
+    if (state === GameState.Draw) {
+        $("#whosturn").text("Draw")
+    } else if (turn % 2 != 0) {
+        $("#whosturn").text("Yellow Wins!")
+    } else {
+        $("#whosturn").text("Red Wins!")
+    }
+
 }
